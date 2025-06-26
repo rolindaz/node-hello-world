74863499ce28bd18c8df21c9c5588ce6b8b6ebda
rolindaz <rolinda.zamboni@gmail.com>
2025-06-26 13:38:11 +0200

    import axios in index.js. Call photo blog endpoint through axios to return api data

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-console.log('Hello Boolean');
-
 import chalk from 'chalk';
 
+import axios from 'axios';
+
+console.log('Hello Boolean');
+
 console.log(chalk.bgRed(process.env.SUPER_SECRET_KEY));
+
+axios.get('https://lanciweb.github.io/demo/api/pictures/')
+.then(function (response) {
+    console.log(response.data);
+});
